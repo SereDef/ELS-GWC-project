f72881377e7a0fe5797cb0711f88cf9d4aad25c4
SereDef <sere.def@gmail.com>
2024-06-21 11:04:31 +0200

    added supp table 1

--- 4.descriptives.R
+++ 4.descriptives.R
@@ -15,14 +15,14 @@ print(cor(complete(sample,0)[c('prenatal_stress', 'postnatal_stress')], use='pai
 preposcor <- miceadds::micombine.cor(sample, variables=c('prenatal_stress', 'postnatal_stress'))
 print(preposcor)
 
-ld <- complete(sample, action = 'long', include = FALSE)
+ld <- mice::complete(sample, action = 'long', include = FALSE)
 
 # Summary of continuous variables = median (min, max)
 cont_summ <- function(cont_vars) {
   summ <- ld %>% 
     select(.imp, all_of(cont_vars)) %>%
     group_by(.imp) %>%
-    summarise_all(list('med'=median, 'min'=min, 'max'=max)) %>%
+    summarise_all(list('med'=median, 'min'=min, 'max'=max), na.rm=TRUE) %>%
     summarise_at(vars(-.imp), mean) %>% 
     round(., 2) %>% 
     matrix(nrow=length(cont_vars), ncol=3, dimnames = list(cont_vars, c('med','min','max'))) %>% 
@@ -54,4 +54,40 @@ total_summ <- rbind(summ1,summ2)
 
 write.csv(total_summ, paste0(project_dir,'/Table1.csv'))
 
+
+# Add complete dateset descriptives for supplement =======================================================================================
+full_data <- readRDS(file.path(project_dir, "Data/ELS_brain.rds"))
+
+cat(nrow(complete(full_data,0)))
+
+ld <- mice::complete(full_data, action = 'long', include = FALSE) %>% 
+   # Recode ethnicity 
+   mutate(ethnicity = if_else(is.na(ethn_cont), NA_character_,
+                      if_else(ethn_cont=="Dutch", "Dutch", 
+                      if_else(ethn_cont=="European", "other European", "non-European"))),
+                      # NOTE: non-European = 'Indonesian','American, western','Asian, western','Oceanie','Cape Verdian','Moroccan','Dutch Antilles','Surinamese','Turkish','African','American, non western','Asian, non western'
+          sex = factor(sex, labels=c("Male", "Female")),
+          postnatal_stress_cat = relevel(cut(postnatal_stress, quantile(postnatal_stress, na.rm=TRUE)[-3], include.lowest=TRUE, 
+                                            labels=c("Low","Medium","High")), ref="Medium"),
+          prenatal_stress_cat = relevel(cut(prenatal_stress, quantile(prenatal_stress, na.rm=TRUE)[-3], include.lowest=TRUE, 
+                                            labels=c("Low","Medium","High")), ref="Medium")) %>%
+
+   # Rename some variables for easier reading
+   rename(., any_of(c(age = "age_child_mri_f09", 
+                      prenatal_smoking = "m_smoking", 
+                      prenatal_alcohol = "m_drinking", 
+                      mean_cortical_thickness_lh = "lh_MeanThickness_f09", 
+                      mean_cortical_thickness_rh = "rh_MeanThickness_f09")))
+
+summ1_full <- cont_summ(c('prenatal_stress', 
+                     'pre_life_events','pre_contextual_risk','pre_parental_risk','pre_interpersonal_risk',
+                     'postnatal_stress', 
+                     'post_life_events','post_contextual_risk','post_parental_risk','post_interpersonal_risk','post_direct_victimization', 
+                     'age', 'mean_cortical_thickness_lh', 'mean_cortical_thickness_rh'))
+
+summ2_full <- do.call('rbind', lapply(c('sex', 'ethnicity', 'prenatal_smoking', 'prenatal_alcohol', 'm_education'), cate_summ))
+
+total_summ_full <- cbind(total_summ, rbind(summ1_full, summ2_full))
+
+write.csv(total_summ_full, paste0(project_dir,'/SuppTable1.csv'))
 
